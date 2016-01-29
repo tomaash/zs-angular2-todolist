e7b93a53c9c19128cb907feffc499c1ec6c60e4e
Tomas Holas <tomas.holas@gmail.com>
2016-01-29 18:07:47 +0100

    remove observable and | async from todos service, error handling

--- src/app/providers/todos-service.ts
+++ src/app/providers/todos-service.ts
@@ -4,30 +4,45 @@ import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class TodosService {
-  list: Observable<Array<Object>>;
-  local: Observable<Array<Object>>;
+  list: Array<Object>;
   current = '';
+  busy: Boolean = false;
+  error: Boolean = false;
   constructor(private http: Http) {
     this.reload();
   }
 
   reload() {
-    this.list = this.http.get('https://zs-todo-api.herokuapp.com/todos')
-      .map(data => data.json());
-  }
+    this.http.get('https://zs-todo-api.herokuapp.com/todos')
+      .subscribe(
+        data => { this.list = data.json() },
+        err => console.log(err)
+      );
+    }
 
   addTodo() {
     var headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    var payload = JSON.stringify({name: this.current});
+    var nextTodo = {name: this.current};
+    var payload = JSON.stringify(nextTodo);
+    this.busy = true;
+    this.error = false;
+    this.list.unshift(nextTodo)
     this.http.post('https://zs-todo-api.herokuapp.com/todos',
       payload,
       {headers}
       ).subscribe(
-        data => this.reload(),
-        err => console.log(err),
-        () => { this.current = ''; }
+        data => {
+          console.log(data);
+          this.busy = false;
+          this.current = '';
+        },
+        err => {
+          this.error = true;
+          this.busy = false;
+          this.list.shift();
+          console.log(err)
+        }
       );
   }
-
 }
